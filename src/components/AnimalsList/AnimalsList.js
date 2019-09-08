08a047c738a10ac791507593c6cac1d3559c1dc7
Simone Boscolo Berto <simone.boscoloberto2@ibm.com>
2019-09-09 00:55:12 +0100

    [Closes #7] Added dialog to insert new animal by type

--- src/components/AnimalsList/AnimalsList.js
+++ src/components/AnimalsList/AnimalsList.js
@@ -6,6 +6,7 @@ import Button from '@material-ui/core/Button';
 import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
 import AnimalGlobal from './components/AnimalGlobal';
 import AnimalSpecific from './components/AnimalSpecific';
+import AddAnimalDialog from './components/AddAnimalDialog';
 import animalsList from './data';
 import {
   getAnimalsTypeList,
@@ -33,28 +34,48 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const AnimalsList = () => {
+  // State to handle total list of animals on each update
   const [totalAnimalsList, setTotalAnimalsList] = useState(animalsList);
   const [isSpecific, setIsSpecific] = useState(false);
   const classes = useStyles();
 
+  // State to handle global and specific list
   const globalAnimalsType = getAnimalsTypeList(totalAnimalsList);
   const [globalList, setGlobalList] = useState(globalAnimalsType);
   const [specificList, setSpecificList] = useState([]);
+
+  // State to handle add new type of animal
+  const [animalType, setAnimalType] = React.useState({});
+  const [open, setOpen] = React.useState(false);
 
   const handleSearch = e => {
     console.log('Something typed', e);
   };
 
+  // Move to specific list of animals
   const handleSelectedType = type => {
     const animalsByType = getAnimalsByType(totalAnimalsList, type);
     setSpecificList(animalsByType);
     setIsSpecific(true);
   };
 
-  const addAnimal = () => {
-    console.log('Adding a new animal');
+  // Handlers to add new animals
+  const handleOpenDialog = animal => {
+    setOpen(true);
+    setAnimalType(animal);
   };
 
+  const handleAddAnimal = newAnimal => {
+    console.log('Adding a new animal', newAnimal);
+    setOpen(false);
+    const newTotalAnimalsList = totalAnimalsList.concat([newAnimal]);
+    setTotalAnimalsList(newTotalAnimalsList);
+    // Recreating the global list
+    const globalAnimalsType = getAnimalsTypeList(newTotalAnimalsList);
+    setGlobalList(globalAnimalsType);
+  };
+
+  // Handler to remove an animal for both lists
   const removeAnimal = animal => {
     console.log('Removing animal', animal);
     // Removing animal from the total list of animals
@@ -66,11 +87,12 @@ const AnimalsList = () => {
     // Recreating the specific list
     const animalsByType = getAnimalsByType(newAnimalsList, animal.type);
     if (!animalsByType.length) {
-        setIsSpecific(false);
+      setIsSpecific(false);
     }
     setSpecificList(animalsByType);
   };
 
+  // Go back to Global list of animals
   const handleBack = () => {
     const globalAnimalsType = getAnimalsTypeList(totalAnimalsList);
     setGlobalList(globalAnimalsType);
@@ -102,7 +124,7 @@ const AnimalsList = () => {
                 isSpecific={isSpecific}
                 animal={animal}
                 onTypeClick={handleSelectedType}
-                onAdd={addAnimal}
+                onAdd={handleOpenDialog}
               />
             </Grid>
           ))}
@@ -113,6 +135,12 @@ const AnimalsList = () => {
             </Grid>
           ))}
       </Grid>
+      <AddAnimalDialog
+        open={open}
+        animalType={animalType}
+        onAddAnimal={handleAddAnimal}
+        onClose={() => setOpen(false)}
+      />
     </div>
   );
 };
